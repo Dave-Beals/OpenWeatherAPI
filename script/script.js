86afe83ad2272e775221949b118ef854bc5f6600
Dave <dbeals85@gmail.com>
2017-07-02 22:49:37 -0700

    added sound effects for button clicks

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function getWeatherSeattle() {
+  let seattleSound = document.getElementById("weatherSound");
+  seattleSound.play();
+}
+
+function getWeatherLondon() {
+  let londonSound = document.getElementById("weatherSound");
+  londonSound.play();
+}

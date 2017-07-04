f8ebbccf52dd103ad67a81fa26821d635cfa95b5
Dave <dbeals85@gmail.com>
2017-07-03 19:31:06 -0700

    changed buttons to radio form and commented out sound effect function for now

--- script/script.js
+++ script/script.js
@@ -1,9 +1,32 @@
-function getWeatherSeattle() {
-  let seattleSound = document.getElementById("weatherSound");
-  seattleSound.play();
+const apiURL = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather"
+//need to input key somehow
+const APPID = "b113558884743a089516dcd3172bc769"
+
+function getWeatherSeattle(queryString) {
+  // weatherSound.play()
+  let request = new XMLHttpRequest()
+
+  //starts talking to API - 3 params, which are
+  //request method, url (optional?) async flag (default true)
+  request.open("GET", apiURL, true)
+
+  //fires when the request is complete
+  //long term - update the DOM
+  //short term - show me what I've got
+  request.onload = function() {
+    console.log(request)
+  }
+
+  //fires if something goes wrong
+  request.error = function(errorObject) {
+    console.log("bwoken")
+    console.log(errorObject)
+  }
+
+  //send the request
+  request.send()
 }
 
 function getWeatherLondon() {
-  let londonSound = document.getElementById("weatherSound");
-  londonSound.play();
+  // weatherSound.play()
 }

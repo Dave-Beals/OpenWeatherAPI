83559c77643b87fedd2f4658d523bf8ddef2f555
Dave <dbeals85@gmail.com>
2017-07-04 16:31:34 -0700

    API not working, either getting 404 not authorized or no errors but no response

--- script/script.js
+++ script/script.js
@@ -1,66 +1,60 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather"
+const apiURL = "http://api.openweathermap.org/data/2.5/weather";
 //need to input key somehow
-const appId = "b113558884743a089516dcd3172bc769"
-const form = document.querySelector("form")
-const seattleCoord = {lat: 47.6762, lon: -122.3182}
-const londonCoord = {lat: 51.5074, lon: 0.1278}
-var selectedCity = ''
+const appId = "b113558884743a089516dcd3172bc769";
+const form = document.querySelector("form");
+const seattleCoord = {lat: 47.6762, lon: -122.3182};
+const londonCoord = {lat: 51.5074, lon: 0.1278};
+var selectedCity = '';
 
 
-let debug = null
+let debug = null;
 
 function handleSubmit() {
-  event.preventDefault()
-  console.log(form)
+  event.preventDefault();
+  console.log(form);
   //get form values
-  var cityPick = form.citySelect.value //this will make value the city selected
-    if(cityPick === "Seattle") {
-      selectedCity = seattleCoord
+  var cityPick = form.citySelect.value; //this will make value the city selected
+    if(form.citySelect.value === "Seattle") {
+      cityPick = seattleCoord;
     } else {
-      selectedCity = londonCoord
+      cityPick = londonCoord;
     }
-    console.log(selectedCity)
-    return selectedCity
+    //serialize them into a query string
+    let queryString = queryBuilder(cityPick);
+    //call getWeather with the query string
+    getWeather(queryString);
+  }
+
   //put values into a string
 
   // if (selectedCity === "Seattle") {
-  //   queryString = '${apiURL}?{seattleCoord}&APPID=${appId}'
+  //   queryString = `?${apiURL}?${seattleCoord}&APPID=${appId}`
   // } else {
-  //   queryString = '${apiURL}?{londonCoord}&APPID=${appId}'
+  //   queryString = `?${apiURL}?${londonCoord}&APPID=${appId}`
   // }
   // return queryString
-  let queryString = queryBuilder(selectedCity)
-  //call getWeather with the query string
-  getWeather(queryString)
-}
-
 
 
 function getWeather(queryString) {
-  // weatherSound.play()
-  let request = new XMLHttpRequest()
-
-  //starts talking to API - 3 params, which are
-  //request method, url (optional?) async flag (default true)
-  request.open("GET", apiURL + queryString, true)
+  let request = new XMLHttpRequest();
+  //starts talking to API - 3 params
+  request.open("GET", apiURL + queryString, true);
   //fires when the request is complete
   //long term - update the DOM
   //short term - show me what I've got
   request.onload = function() {
-    // let reportDiv = document.getElementById("weatherResults")
-    let response = JSON.parse(request.response)
-    console.log(response.body)
+    let response = JSON.parse(request.response);
+    let temperature = response.main.temp;
 
-  //COMMENTING BELOW TEMPORARILY______________
-  // request.onload = function() {
-  //   let reportDiv = document.getElementById("weatherResults")
-  //   let response = JSON.parse(request.response)
-  //   console.log(response.body)
-  // }
+    let reportDiv = document.getElementById("weatherResults");
+    var p = document.createElement("p");
+    p.innerHTML = "The temperature is" + temperature;
+    reportDiv.appendChild(p);
+  }
 
-  //fires if something goes wrong - THIS CAN BE DONE WITH A FRAMEWORK DOWN THE ROAD
+  //fires if something goes wrong
   request.error = function(errorObject) {
-    console.log("bwoken")
+    console.log("Ruh roh")
     console.log(errorObject)
   }
 
@@ -83,18 +77,16 @@ function getWeather(queryString) {
 
 
 function queryBuilder(queryObj) {
-  let holder = []
+  let holder = [];
   //loop through key value pairs
   for(let key in queryObj){
     //turn each on into "key-value"
-       let convert = `${encodeURIComponent(key)}=${encodeURIComponent(queryObj[key])}`
+       let convert = `${encodeURIComponent(key)}=${encodeURIComponent(queryObj[key])}`;
        // encodeURIComponent converts spaces and & to URI friendly values so we don't have to worry about them
-       holder.push(convert)
+       holder.push(convert);
      }
 //      // concatenate the pairs together, with & between
-     let longString = holder.join("&")
+     let longString = holder.join("&");
 //      // prepend a ? to concatenated string, return. Also append w/ lat long and appID
-     return `?${longString}&${appId}`
-     console.log(`?${longString}&${appId}`)
+     return `?${longString}&${appId}`;
    }
-}

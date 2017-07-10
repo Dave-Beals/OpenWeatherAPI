571b75c75027d0e4d2e070698a96096b473f5c47
Dave <dbeals85@gmail.com>
2017-07-10 10:57:47 -0700

    success in getting user loc as object of lat and long. Not able to feed those to weather API yet

--- script/script.js
+++ script/script.js
@@ -4,8 +4,8 @@ const appId = "APPID=b113558884743a089516dcd3172bc769";
 const form = document.querySelector("form");
 const seattleCoord = {lat: 47.6762, lon: -122.3182};
 const londonCoord = {lat: 51.5074, lon: 0.1278};
-var selectedCity = '';
-var audio = document.getElementById("weatherSound")
+let newLoc = [];
+var audio = document.getElementById("weatherSound");
 
 
 let debug = null;
@@ -13,12 +13,29 @@ let debug = null;
 function handleSubmit() {
   event.preventDefault();
   console.log(form);
+  
+  function newLocation() {
+    navigator.geolocation.getCurrentPosition(geolocSuccess, geolocError);
+  }
+
+  function geolocSuccess(position) {
+    const newPos = {lat: position.coords.latitude, lng: position.coords.longitude};
+    newLoc.push(newPos);
+    console.log(newPos);
+    //getLocation(newPos);
+  }
+
+  function geolocError(){
+    console.log("Error getting user's location")
+
   //get form values
   var cityPick = form.citySelect.value; //this will make value the city selected
     if(form.citySelect.value === "Seattle") {
       cityPick = seattleCoord;
-    } else {
+    } else if(form.citySelect.value === "London") {
       cityPick = londonCoord;
+    } else if(form.citySelect.value === "userLoc") {
+      cityPick = newLoc;
     }
     //serialize them into a query string
     let queryString = queryBuilder(cityPick);
@@ -27,15 +44,45 @@ function handleSubmit() {
     audio.play();
   }
 
-  //put values into a string
+// function newLocation() {
+//   navigator.geolocation.getCurrentPosition(geolocSuccess, geolocError);
+// }
+//
+// function geolocSuccess(position) {
+//   const newPos = {lat: position.coords.latitude, lng: position.coords.longitude};
+//   newLoc.push(newPos);
+//   console.log(newPos);
+//   //getLocation(newPos);
+// }
+//
+// function geolocError(){
+//   console.log("Error getting user's location")
+// }
 
-  // if (selectedCity === "Seattle") {
-  //   queryString = `?${apiURL}?${seattleCoord}&APPID=${appId}`
-  // } else {
-  //   queryString = `?${apiURL}?${londonCoord}&APPID=${appId}`
-  // }
-  // return queryString
-
+// function onerrorFunc(){
+//   printListItem("Sorry, an error occured");
+// }
+//
+// // function getLocation(locObj) {
+// //   let mapUri = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${locObj.lat},${locObj.lng}&key=AIzaSyA5d8ZwO3RgYJsVYzTKue_IuBK0nxTfeFY`;
+// //   let request = new XMLHttpRequest();
+// //   request.open("GET", mapUri, true);
+// //   //create onload func
+// //   request.onload = onloadFunc;
+// //   //tie to existing onerror
+// //   request.onerror = onerrorFunc;
+// //   request.send();
+// // }
+// //
+// // function onloadFunc(){
+// //   const resp = JSON.parse(this.response);
+// //   console.log(resp);
+// //   if(resp.results.length>0){
+// //     printListItem(resp.results[0].formatted_address);
+// //   } else {
+// //     printListItem("No results were found");
+// //   }
+// // }
 
 function getWeather(queryString) {
   let request = new XMLHttpRequest();
@@ -44,6 +91,7 @@ function getWeather(queryString) {
   //fires when the request is complete
   //long term - update the DOM
   //short term - show me what I've got
+  //tweak below to start new function that can serve Seattle, London, AND userLoc
   request.onload = function() {
     let response = JSON.parse(request.response);
     let temperature = response.main.temp;
@@ -56,7 +104,7 @@ function getWeather(queryString) {
 
   //fires if something goes wrong
   request.error = function(errorObject) {
-    console.log("Ruh roh")
+    console.log("Ah jeez.")
     console.log(errorObject)
   }
 
@@ -65,18 +113,9 @@ function getWeather(queryString) {
 }
 
 
-// function queryBuilder(queryObj) {
-//   if(form.citySelect.value === "Seattle") {
-//     return '${apiURL}?{seattleCoord}&APPID=${appId}'
-//   } else {
-//     return '${apiURL}?{londonCoord}&APPID=${appId}'
-//   }
-// }
-
-//REPLACE ALL THIS WITH AN EXPLICITLY STATED IF/ELSE
 //OpenWeatherMap's API call for geo coord: api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon} ADD KEY
 
-//  http://api.openweathermap.org/data/2.5/weather?lat=51.5074&lon=0.1278&b113558884743a089516dcd3172bc769
+
 
 function queryBuilder(queryObj) {
   let holder = [];
